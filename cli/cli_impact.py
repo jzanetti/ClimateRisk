26735a073019941d83ca6b1fb23887fe8ed8947d
Sijin Zhang <zsjzyhzp@gmail.com>
2022-12-21 14:57:26 +1300

    update climate

--- cli/cli_impact.py
+++ cli/cli_impact.py
@@ -4,7 +4,9 @@ import numpy as np
 road_path = "/Users/zhans/Github/ClimateRisk/exp/hello_world/lds-nz-road-centrelines-topo-150k-SHP/nz-road-centrelines-topo-150k.shp"
 wellington_roads = gpd.read_file(road_path)
 wellington_roads = wellington_roads.to_crs(epsg=4326)
-exp_gpd = Exposures(wellington_roads)
-exp_gpd.gdf["impf_WS"] = 1
-exp_gpd.gdf["value"] = 1
-exp_gpd.gdf.plot('value', cmap='inferno')
+wellington_roads = Exposures(wellington_roads)
+wellington_roads.gdf["impf_WS"] = 1
+wellington_roads.gdf["value"] = wellington_roads.gdf["lane_count"]
+# exp_gpd.gdf.plot('value', cmap='inferno')
+
+
